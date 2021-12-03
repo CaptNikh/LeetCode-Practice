8f664443b0cacca007e8a359179a7ea8d461e3c7
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2021-12-04 01:11:37 +0530

    Time: 16 ms (85.04%), Space: 18.1 MB (19.55%) - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     int missingNumber(vector<int>& arr) {
         int n =  arr.size();
-        for(int i = 0; i < n; i++)
+//         for(int i = 0; i < n; i++)
+//         {
+//             while(arr[i] != n && i != arr[i])
+//             {
+//                 swap(arr[i], arr[arr[i]]);
+//             }
+//         }
+        
+//         int ans = n;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(i != arr[i])
+//             {
+//                 ans = i;
+//                 break;
+//             }
+//         }
+        
+//         return ans;
+//         using XOR operatot
+        int i = 0, ans = 0;
+        
+        for(i = 0; i < n; i++)
         {
-            while(arr[i] != n && i != arr[i])
-            {
-                swap(arr[i], arr[arr[i]]);
-            }
+            ans = ans^i^arr[i];
         }
         
-        int ans = n;
-        for(int i = 0; i < n; i++)
-        {
-            if(i != arr[i])
-            {
-                ans = i;
-                break;
-            }
-        }
-        
-        return ans;
+        return ans^i;
     }
 };
