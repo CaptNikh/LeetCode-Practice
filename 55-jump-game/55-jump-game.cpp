beb4d06f6c4591b377bc05232066afb5125e2715
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2022-01-06 01:35:19 +0530

    Time: 107 ms (28.60%), Space: 48.1 MB (94.61%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    bool canJump1(vector<int> &arr, int i, vector<int> &dp)
-    {
-        if(i >= arr.size() - 1)
-            return true;
-        if(arr[i] == 0)
-            return false;
-        if(dp[i] != -1)
-            return dp[i];
-        bool ans = false;
-        for(int j = arr[i]; j >= 1; j--)
-        {
-            ans |=  canJump1(arr, i + j, dp);
-            if(ans == true)
-                break;
-        }
-        if(ans)
-            dp[i] = 1;
-        else
-            dp[i] = 0;
-        return ans;
-    }
-    
     bool canJump(vector<int>& nums) {
-        if(nums.size() == 1)
+        int n = nums.size();
+        
+        int  minJump = 0;
+        
+        for(int i = n - 2; i >= 0; i--)
+        {
+            minJump++;
+            if(nums[i] >= minJump)
+            { 
+                minJump = 0;
+            }
+        }
+        
+        if(minJump == 0)
             return true;
-        vector<int>dp(nums.size() , -1);
-        return canJump1(nums, 0, dp);
+        return false;
     }
 };
