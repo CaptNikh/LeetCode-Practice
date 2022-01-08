c2779b2ba22f76ca35f3636b73bcc0755b94e446
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2022-01-09 00:24:50 +0530

    Time: 4 ms (26.37%), Space: 8.9 MB (7.14%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
+    int LCS(string &s, string &t, int i , int j,vector<vector<int>> &dp)
+    {
+        if(i == s.length() || j == t.length())
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(s[i] == t[j])
+            return dp[i][j] = 1 + LCS(s, t, i + 1, j + 1, dp);
+        
+        int x = LCS(s, t, i, j + 1, dp);
+        int y = LCS(s, t, i + 1, j, dp);
+        
+        return dp[i][j] = max(x , y);
+    }
     bool isSubsequence(string s, string t) {
 //      we can do the LCS approach and if LCS == s.lenght then it is true
-        int n = s.length();
-        int m = t.length();
-        int i = 0, j = 0;
+        int n = s.length(), m = t.length();
         
-        while(i < n && j < m)
-        {
-            if(s[i] == t[j])
-            {
-                i++;
-                j++;
-            }
-            else
-            {
-                j++;
-            }
-        }
-        if(i == n)
-            return true;
+        vector<vector<int>> dp(n , vector<int> (m, -1));
+       if(s.length() == LCS(s, t, 0 , 0, dp))
+           return true;
         else
             return false;
     }
