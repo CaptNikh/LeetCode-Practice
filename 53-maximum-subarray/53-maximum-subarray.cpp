d1ecca3bf97291a0fa52ff2f1b7c5ccb0dcb5b76
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2022-01-10 00:52:25 +0530

    Time: 245 ms (5.12%), Space: 67.7 MB (53.90%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1)
-            return nums[0];
-        vector<int> dp(n);
+      int n = nums.size();
         
-        dp[0] = nums[0] < 0 ? 0 : nums[0];
-        int maxSoFar = dp[0];
-        for(int i = 1; i < n; i++)
-        {
-            dp[i] = dp[i - 1] + nums[i];
-            
-            if(dp[i] < 0)
-                dp[i] = 0;
-            
-            maxSoFar = max(dp[i], maxSoFar);
-        }
         int x = *max_element(nums.begin(), nums.end());
-        
         if(x < 0)
-            return x;
+            return x;                             
+        int curr_sum = 0; 
+        int maxSoFar = 0;
         
+        for(int i = 0; i < n; i++)
+         {
+            curr_sum += nums[i];
+             
+             if(curr_sum < 0)
+                 curr_sum = 0;
+             
+             maxSoFar = max(maxSoFar, curr_sum);
+         }
         return maxSoFar;
         
     }
