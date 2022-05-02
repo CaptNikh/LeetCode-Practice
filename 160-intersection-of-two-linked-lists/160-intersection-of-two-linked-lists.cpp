15c6c35ee2bcc4b7801145e15e45452930182f25
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2022-05-02 16:47:15 +0530

    Time: 66 ms (41.11%), Space: 14.5 MB (89.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,23 +9,57 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+//         0 0 0 0 
+                 // 0   
         
         
-        unordered_set<ListNode*> s;
         ListNode* curr = headA;
         while(curr != NULL) {
-            s.insert(curr);
+            cnt1++;
+            curr = curr->next;
+        }
+        curr = headB;
+        while(curr != NULL) {
+            cnt2++;
             curr = curr->next;
         }
         
-        
-        curr = headB;
-        
-        while(curr != NULL)
-        {
-            if(s.find(curr) != s.end())
-                return curr;
-            curr = curr->next;
+        if(cnt1 > cnt2) {
+            int a = cnt1 - cnt2;
+            
+            curr = headA;
+            while(a--)
+                curr = curr->next;
+            
+            ListNode* curr1 = headB;
+            
+            while(curr!=NULL && curr1 != NULL)
+            {
+                if(curr == curr1)
+                    return curr;
+                curr = curr->next;
+                curr1 = curr1->next;
+            } 
+        }
+        else {
+            int a = cnt2 - cnt1;
+            
+            curr = headB;
+            while(a--)
+                curr = curr->next;
+            
+            ListNode* curr1 = headA;
+            
+            while(curr!=NULL && curr1 != NULL)
+            {
+                if(curr == curr1)
+                    return curr;
+                curr = curr->next;
+                curr1 = curr1->next;
+            } 
         }
         
         return NULL;
