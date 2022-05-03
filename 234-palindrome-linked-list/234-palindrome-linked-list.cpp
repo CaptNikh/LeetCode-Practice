385a92817f8b04dec44258daec28e648a8a90801
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2022-05-03 21:12:52 +0530

    Time: 336 ms (33.80%), Space: 114 MB (85.26%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,24 +10,68 @@
  */
 class Solution {
 public:
+    
+    ListNode* reverse(ListNode* head) {
+    
+        ListNode* curr = head, *prev = NULL;
+        
+        while(curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+        
+    }
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
+        // reverse till the middle of the linked list
+        
+        ListNode* slow = head, *fast = head;
+        
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode* rev = NULL;
+        if(fast == NULL)
+            rev = reverse(slow);
+        else
+            rev = reverse(slow->next);
         
         ListNode* curr = head;
-        while(curr != NULL) {
-            arr.push_back(curr->val);
+        while(rev != NULL) {
+            if(rev->val != curr->val)
+                return false;
+            
+            cout << rev->val << " " << curr->val;
+            
+            rev = rev->next;
             curr = curr->next;
         }
         
+        
+        int count = 0;
         curr = head;
-        int i = arr.size() - 1;
         while(curr != NULL) {
-            if(curr->val != arr[i])
-                return false;
-            
             curr = curr->next;
-            i--;
+            count++;
+            
+            if(count > 2)
+                break;
         }
+        
+        if(count == 2){
+            if(head->val == head->next->val)
+                return true;
+            else
+                return false;
+        }
+        
+        
         return true;
+        
     }
 };
